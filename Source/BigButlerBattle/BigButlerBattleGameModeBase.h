8115053991a3faa58afb6d47b05d73c0361de0f0
Kristoffer Skau <kskau93@gmail.com>
2020-01-06 14:23:22 +0100

    Initial commit 2

---
+++ Source/BigButlerBattle/BigButlerBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BigButlerBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGBUTLERBATTLE_API ABigButlerBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
