b39fed16568e02b32b3884fdea5e33fede85a703
Anders Syvertsen <andesyv@gmail.com>
2020-01-07 19:04:26 +0100

    Overrode default behavior to a custom one
    Made a custom movement mode and temporarily mimicked default walking for custom movement mode.

--- Source/BigButlerBattle/PlayerCharacterMovementComponent.h
+++ Source/BigButlerBattle/PlayerCharacterMovementComponent.h
@@ -6,7 +6,17 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "PlayerCharacterMovementComponent.generated.h"
 
-/**
+// Forward declarations
+class APlayerCharacter;
+
+UENUM(BlueprintType)
+enum class ECustomMovementTypeEnum : uint8
+{
+	MOVE_None			UMETA(DisplayName = "None"),
+	MOVE_Skateboard		UMETA(DisplayName = "Skateboard")
+};
+
+/** Custom override of movement component
  * 
  */
 UCLASS()
@@ -14,10 +24,20 @@ class BIGBUTLERBATTLE_API UPlayerCharacterMovementComponent : public UCharacterM
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Custom Movement")
+	ECustomMovementTypeEnum CurrentCustomMovementMode = ECustomMovementTypeEnum::MOVE_Skateboard;
+
 public:
 	UPlayerCharacterMovementComponent();
 
 protected:
+	void BeginPlay() override;
+
 	void TickComponent(float deltaTime, enum ELevelTick TickType, FActorComponentTickFunction* thisTickFunction) override;
-	
+
+	/** @note Movement update functions should only be called through StartNewPhysics()*/
+	void PhysCustom(float deltaTime, int32 Iterations) override;
+
+	void PhysSkateboard(float deltaTime, int32 Iterations);
 };
