cfc346010bed1bf29dff66da54af04474f7e2d56
Kristoffer Skau <kskau93@gmail.com>
2020-03-06 12:53:13 +0100

    Added extra rooms for 2nd floors for spawning
    - Just need to set it up in game

--- Source/BigButlerBattle/Utils/Spawnpoint.h
+++ Source/BigButlerBattle/Utils/Spawnpoint.h
@@ -10,9 +10,13 @@ UENUM(BlueprintType)
 enum class ERoomSpawn : uint8
 {
 	Room_MainHall		UMETA(DisplayName = "Main Hall"),
+	Room_MainHall2		UMETA(DisplayName = "Main Hall 2nd"),
 	Room_Kitchen		UMETA(DisplayName = "Kitchen"),
+	Room_Kitchen2		UMETA(DisplayName = "Kitchen 2nd"),
 	Room_SquareHall		UMETA(DisplayName = "SquareHall"),
+	Room_SquareHall2	UMETA(DisplayName = "SquareHall 2nd"),
 	Room_StairRoom		UMETA(DisplayName = "StairRoom"),
+	Room_StairRoom2		UMETA(DisplayName = "StairRoom 2nd"),
 	Room_Bar			UMETA(DisplayName = "Bar")
 };
 
